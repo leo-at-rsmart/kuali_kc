c1dc84c4a6909514ec6aabf6854e8537e99c764f
Leo Przybylski <leo@rsmart.com>
2014-06-15 23:03:16 -0700

    # This is a combination of 2 commits. # The first commit's message is:
    Added a new function to handle absence of affiliations.
    Added NPE checks to ensure functionality.
    
    fixes rSmart/issues#221
    fixes rSmart/issues#243
    
    # The 2nd commit message will be skipped:
    
    #	Added a new function to handle absence of affiliations.
    #	Added NPE checks to ensure functionality.
    #
    #	fixes rSmart/issues#221
    #	fixes rSmart/issues#243

--- src/main/java/com/rsmart/kuali/kra/web/jstl/PortalFunctions.java
+++ src/main/java/com/rsmart/kuali/kra/web/jstl/PortalFunctions.java
@@ -19,7 +19,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.kuali.rice.kim.api.identity.Person;
-import org.kuali.rice.kim.api.identity.affiliation.EntityAffiliation;
+import org.kuali.rice.kim.api.identity.affiliation.EntityAffiliationContract;
 import org.kuali.rice.kim.impl.identity.PersonImpl;
 import org.kuali.rice.krad.util.GlobalVariables;
 import org.kuali.rice.krad.util.ObjectUtils;
@@ -27,17 +27,32 @@ import org.kuali.rice.krad.util.ObjectUtils;
 public class PortalFunctions {
 
 	//STAFF,FCLTY,AFLT, etc...
+    /**
+     * Determines if the given the currently logged in user has affiliations of the types specified in <code>types</code>
+     * 
+     * @param types comma separated list of affiliation types. It will get split.
+     * @param return true if the currently logged in user has ANY type in <code>types</code>. 
+     */
 	public static boolean showByAffiliateType(String types){
 		boolean success = false;
-		PersonImpl currentUser = (PersonImpl) GlobalVariables.getUserSession().getPerson();
+        if (GlobalVariables.getUserSession() == null) {
+            return false;
+        }
+		final PersonImpl currentUser = (PersonImpl) GlobalVariables.getUserSession().getPerson();
 		
 		if (ObjectUtils.isNull(currentUser)){
 			return false;
 		}
-		List<String> typesList = Arrays.asList(types.split(","));
-		for (int i=0;i< currentUser.getAffiliations().size();i++){
-			if (currentUser.getAffiliations().get(i).isDefaultValue() && currentUser.getAffiliations().get(i).isActive()){
-				if (typesList.contains(currentUser.getAffiliations().get(i).getAffiliationType().getCode())){
+		final List<String> typesList = Arrays.asList(types.split(","));
+
+		for (int i = 0; currentUser.getAffiliations() != null 
+                      && i < currentUser.getAffiliations().size(); i++){
+			if (currentUser.getAffiliations().get(i) != null 
+                && currentUser.getAffiliations().get(i).isDefaultValue() 
+                && currentUser.getAffiliations().get(i).isActive()) {
+                
+				if (currentUser.getAffiliations().get(i).getAffiliationType() != null 
+                    && typesList.contains(currentUser.getAffiliations().get(i).getAffiliationType().getCode())){
 					success = true;
 				}
 				break;
@@ -45,4 +60,29 @@ public class PortalFunctions {
 		}
 		return success;
 	}
+
+    /**
+     * This is not like {@link #showByAffiliateType(String)}. It is not checking if a user has a specific affiliation. Rather
+     * it checks if the currently logged in user has any affiliation whatsoever. An affiliation is only valid if it is active.
+     *
+     * @return if the currently logged in user has an affiliation at all, or false otherwise.
+     */
+    public static boolean hasAffiliation() {
+        if (GlobalVariables.getUserSession() == null) {
+            return false;
+        }
+
+		final PersonImpl currentUser = (PersonImpl) GlobalVariables.getUserSession().getPerson();
+
+		if (ObjectUtils.isNull(currentUser)){
+			return false;
+		}
+
+        for (final EntityAffiliationContract affiliation : currentUser.getAffiliations()) {
+            if (affiliation != null && affiliation.isActive()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
